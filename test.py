a4e7bf62284fef729c66620efab1b5ec69523a67
banyalshipu <135800237+banyalshipu@users.noreply.github.com>
2023-06-07 11:18:10 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#main.py
+import os
+import tempfile
+from dotenv import load_dotenv
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import OpenAI
+from config import WHITE, GREEN, RESET_COLOR, model_name
+from utils import format_user_question
+from file_processing import clone_github_repo, load_and_index_files
+from questions import ask_question, QuestionContext
