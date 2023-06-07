52efc6c9f0ce36b85cb5fb79594641764529285e
banyalshipu <135800237+banyalshipu@users.noreply.github.com>
2023-06-07 11:23:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,3 +8,4 @@ from config import WHITE, GREEN, RESET_COLOR, model_name
 from utils import format_user_question
 from file_processing import clone_github_repo, load_and_index_files
 from questions import ask_question, QuestionContext
+from questions import ask_question, QuestionContext
